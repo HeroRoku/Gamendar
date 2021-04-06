f4746ad00383786bf673d5b01805c06ff9709aff
Hero <heroroku@gmail.com>
2021-04-06 16:25:13 -0400

    established store setup

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import configureStore from "./store";
+import { Provider } from "react-redux";
+
+const store = configureStore({})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
